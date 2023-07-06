ccd6bee15a0daa1f84fd7f694da7bdadfd01faa4
zpran <zanna@prane.org>
2023-07-06 12:51:50 +0100

    feat: exercise 002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,34 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  return person.city === "Manchester";
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+  if (people === undefined) throw new Error("people is required");
+  return Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+  if (arr === undefined) throw new Error("arr is required");
+  let counter = 0;
+  arr.map((el) => {
+    if (el === "sheep") counter++;
+  });
+  return counter;
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  if (
+    person.address.postCode[0] == "M" &&
+    person.address.city == "Manchester"
+  ) {
+    return true;
+  }
+  return false;
 }
